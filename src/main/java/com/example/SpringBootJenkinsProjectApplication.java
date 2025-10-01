2f3cf26c722d442b7c4ff4483ad95905e6a5387b
shubham <ingaleshubham690@gmail.com>
2025-10-01 16:28:22 +0530

    fff

--- src/main/java/com/example/SpringBootJenkinsProjectApplication.java
+++ src/main/java/com/example/SpringBootJenkinsProjectApplication.java
@@ -5,18 +5,25 @@ import java.util.List;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class SpringBootJenkinsProjectApplication {
+public class SpringBootJenkinsProjectApplication extends SpringBootServletInitializer {
 
-	 @GetMapping("/hello")
-	    public List<String> hello() {
-	        return Arrays.asList("shubham","ramesh","amit");
-	    }
-	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootJenkinsProjectApplication.class);
+    }
+
+	@GetMapping("/hello")
+	public List<String> hello() {
+		return Arrays.asList("shubham", "ramesh", "amit");
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJenkinsProjectApplication.class, args);
 	}
